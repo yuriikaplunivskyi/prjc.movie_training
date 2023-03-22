30c8a71ae3bde79213335ce6e2de53ce2d7f9bfa
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-03-22 13:52:40 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'),
+          poster = document.querySelector('.promo__bg'),
+          genre = poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
+    
+    
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if(newFilm){
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+    
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+    } )
+    
+    const deleteAdv = (arr) => {
+        arr.forEach( function (item) {
+            item.remove();
+        });
+        /* adv.forEach(item => {
+        item.remove();
+        }); */
+    }
+
+
+    
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+        poster.style.backgroundImage = "url('img/bg.jpg')";
+    }
+
+    
+
+    const sortArr = (arr) => {
+        arr.sort();
+    }
+    
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+            </li>
+            `;
+        })
+    }
+
+    makeChanges();
+    deleteAdv(adv)
+    createMovieList(movieDB.movies, movieList)
+})
+
+
+
